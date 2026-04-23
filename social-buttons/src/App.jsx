804507a2c0469dbb23a859c738e9d8b44b654628
Ganeshbhargav <ganeshbhargav619@gmail.com>
2026-04-24 00:59:40 +0530

    Build Social-buttons icons using React components and props

---
+++ social-buttons/src/App.jsx
@@ -0,0 +1,7 @@
+import SocialButtons from "./components/SocialButtons";
+
+function App(){
+  return <SocialButtons />
+}
+
+export default App
---
+++ social-buttons/src/components/SocialButtons.jsx
@@ -0,0 +1,15 @@
+function SocialButtons(){
+    return (
+        <div className="bg-container">
+            <h1 className="heading">Social Buttons</h1>
+
+            <div className="buttons-container">
+                <button className="button like-btn">Like</button>
+                <button className="button comment-btn">Comment</button>
+                <button className="button share-btn">Share</button>
+            </div>
+        </div>
+    )
+}
+
+export default SocialButtons
